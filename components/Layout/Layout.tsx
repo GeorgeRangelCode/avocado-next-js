b2b9bf3270d08495fab8ccb87e64629a07eb00e7
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-08-16 08:52:37 -0500

    CSS en NextJS y su flexibilidad

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from '@components/Navbar/Navbar'
+// import Styles from './layout.module.css'
+// CSS Module
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div>
       <Navbar />
       {children}
-      <footer>This is the footer</footer>
+      <footer className="container">This is the footer</footer>
+      <style jsx>{`
+        div {
+          background-color: salmon;
+        }
+      `}</style>
     </div>
   )
 }
